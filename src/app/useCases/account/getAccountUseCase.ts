4dcf42cacc9582ae47b3c2871db3d952672657ff
Diego Lopes <503598-diegolopes@users.noreply.gitlab.com>
2024-06-22 00:19:33 -0300

    refactor: add Either pattern to GetAccountUseCase to improve the readibility

--- src/app/useCases/account/getAccountUseCase.ts
+++ src/app/useCases/account/getAccountUseCase.ts
@@ -1,32 +1,41 @@
+import { Either, left, right } from "../../../domain/errors/either/either";
 import { AccountRepository } from "../../../domain/repositories/accountRepository";
 import UseCase from "../../../domain/useCases/account/useCase";
 
 type Input = { accountId: string };
-type Output = { accountId: string; name: string; balance: number } | null;
+type Output = Either<
+  { accountId: string; name: string; balance: number } | null,
+  Error
+>;
 
 export class GetAccountUseCase implements UseCase<Input, Output> {
   constructor(private readonly accountRepository: AccountRepository) {}
 
   async execute(input: Input): Promise<Output> {
+    const { accountId } = input;
+
+    if (!accountId) {
+      return left(new Error("No AccountID is given"));
+    }
+
     const accounts = await this.accountRepository.getAll();
 
     if (!accounts || accounts.length > 0) {
-      return null;
+      return right(null);
     }
 
     const accountIdx = accounts.findIndex((acc) => acc.id === input.accountId);
 
     if (accountIdx == -1) {
-      return null;
+      return right(null);
     }
 
     const { name, balance } = accounts[accountIdx];
-    const { accountId } = input;
 
-    return {
+    return right({
       accountId,
       name,
       balance,
-    };
+    });
   }
 }
