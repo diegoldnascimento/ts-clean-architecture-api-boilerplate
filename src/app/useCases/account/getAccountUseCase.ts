a28779f61f8c4d9689b0ff31b002f400834a9c51
Diego Lopes <503598-diegolopes@users.noreply.gitlab.com>
2024-06-22 00:16:50 -0300

    refactor: improve the GetAccountUseCase implementation

--- src/app/useCases/account/getAccountUseCase.ts
+++ src/app/useCases/account/getAccountUseCase.ts
@@ -2,12 +2,31 @@ import { AccountRepository } from "../../../domain/repositories/accountRepositor
 import UseCase from "../../../domain/useCases/account/useCase";
 
 type Input = { accountId: string };
-type Output = { accountId: string; name: string; balance: number };
+type Output = { accountId: string; name: string; balance: number } | null;
 
 export class GetAccountUseCase implements UseCase<Input, Output> {
   constructor(private readonly accountRepository: AccountRepository) {}
 
-  execute(accountId: string): Output {
-    return this.accountRepository.getAll();
+  async execute(input: Input): Promise<Output> {
+    const accounts = await this.accountRepository.getAll();
+
+    if (!accounts || accounts.length > 0) {
+      return null;
+    }
+
+    const accountIdx = accounts.findIndex((acc) => acc.id === input.accountId);
+
+    if (accountIdx == -1) {
+      return null;
+    }
+
+    const { name, balance } = accounts[accountIdx];
+    const { accountId } = input;
+
+    return {
+      accountId,
+      name,
+      balance,
+    };
   }
 }
